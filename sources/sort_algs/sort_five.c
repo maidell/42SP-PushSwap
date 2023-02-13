6b0b3a7aa383d21a983eab45427f3a5e5123b9a7
maidell <mateusmaidel@gmail.com>
2023-02-13 11:34:46 -0300

    Fix norminette

--- sources/sort_algs/sort_five.c
+++ sources/sort_algs/sort_five.c
@@ -6,7 +6,7 @@
 /*   By: mmaidel- <mmaidel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 11:21:52 by mmaidel-          #+#    #+#             */
-/*   Updated: 2023/02/13 11:32:30 by mmaidel-         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:34:24 by mmaidel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,27 +75,14 @@ void	sort_five(t_stack *stack_a, t_stack *stack_b)
 	if (check_sort_stack(stack_a) == 0)
 	{
 		if (stack_a->top->index == 4)
-		{
-			pb(stack_a, stack_b);
-			sort_four(stack_a, stack_b);
-			pa(stack_a, stack_b);
-			ra(stack_a);
-		}
+			update_five_sorts(stack_a, stack_b);
 		else if (stack_a->bottom->index == 4)
-		{
 			utils_sort_five(stack_a, stack_b);
-		}
 		else if (stack_a->bottom->next->index == 4)
-		{
 			utils_sort_five(stack_a, stack_b);
-		}
 		else if (stack_a->top->prev->index == 4)
-		{
 			utils_sort_five(stack_a, stack_b);
-		}
 		else if (stack_a->top->prev->prev->index == 4)
-		{
 			utils_sort_five(stack_a, stack_b);
-		}
 	}
 }
--- sources/sort_algs/utils_less_than_five.c
+++ sources/sort_algs/utils_less_than_five.c
@@ -6,7 +6,7 @@
 /*   By: mmaidel- <mmaidel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:24:17 by mmaidel-          #+#    #+#             */
-/*   Updated: 2023/02/13 11:31:14 by mmaidel-         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:33:33 by mmaidel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,14 @@ void	utils_sort_five(t_stack *stack_a, t_stack *stack_b)
 
 void	update_five_sorts(t_stack *stack_a, t_stack *stack_b)
 {
-	if (stack_a->bottom->next->index == 4)
+	if (stack_a->top->index == 4)
+	{
+		pb(stack_a, stack_b);
+		sort_four(stack_a, stack_b);
+		pa(stack_a, stack_b);
+		ra(stack_a);
+	}
+	else if (stack_a->bottom->next->index == 4)
 	{
 		rra(stack_a);
 		rra(stack_a);
